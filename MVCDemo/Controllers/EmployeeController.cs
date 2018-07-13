18cc6267cebd70c1da87ad4328ba19cb45d63035
mehdibz <bz.mehdi@gmail.com>
2018-07-13 01:37:55 -0700

    View added

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee employee = new Employee();
+            employee.EmployeeId = 101;
+            employee.Name = "John";
+            employee.Gender = "Male";
+            employee.City = "London";
+
+            return View(employee);
+        }
+    }
+}
