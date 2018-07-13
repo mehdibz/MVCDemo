8135eb52c7c04092974dc56a7669900a86b5affd
mehdibz <bz.mehdi@gmail.com>
2018-07-13 00:32:53 -0700

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.countries = new List<string>()
+            {
+                "India","US","UK","Canada"
+            };
+
+            ViewData["MyList"] = new List<string>()
+            {
+                "IRAN","Syria","Russia","====="
+            };
+            return View();
+        }
+    }
+}
